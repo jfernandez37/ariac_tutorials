50c1e2f8b45545ae975b91a8fc4c9bfe395b71db
Joseph Fernandez <jothomas0615@gmail.com>
2023-11-29 09:55:13 -0500

    Collision objects spawn in correctly

--- ariac_tutorials/create_object_yaml.py
+++ ariac_tutorials/create_object_yaml.py
@@ -1,4 +1,22 @@
 import yaml
+import math
+
+def quaternion_from_euler(roll: float, pitch: float, yaw: float) -> list:
+    cy = math.cos(yaw * 0.5)
+    sy = math.sin(yaw * 0.5)
+    cp = math.cos(pitch * 0.5)
+    sp = math.sin(pitch * 0.5)
+    cr = math.cos(roll * 0.5)
+    sr = math.sin(roll * 0.5)
+
+    q = [0] * 4
+    
+    q[0] = cy * cp * sr - sy * sp * cr
+    q[1] = sy * cp * sr + cy * sp * cr
+    q[2] = sy * cp * cr - cy * sp * sr
+    q[3] = cy * cp * cr + sy * sp * sr
+    q = [round(val,4) for val in q]
+    return q
 
 objects_dict = {}
 
@@ -6,7 +24,7 @@ objects_dict = {}
 bin_positions = [[x,y,0.0] 
                  for x in [-1.9,-2.65]
                  for y in [3.375,2.625,-3.375,-2.625]]
-bin_rotations = [[0.0,0.0,0.0,1.0] for _ in range(8)]
+bin_rotations = [quaternion_from_euler(0,0,math.pi) for _ in range(8)]
 for i in range(1,9):
     objects_dict[f"bin{i}"]={"position":bin_positions[i-1],"orientation":bin_rotations[i-1],"file":"bin.stl"}
 
@@ -14,30 +32,29 @@ for i in range(1,9):
 assembly_stations_positions = [[x,y,0.0]
                                for x in [-7.3,-12.3]
                                for y in [3.0,-3.0]]
-assembly_stations_rotations = [[0.0,0.0,0.0,1.0] for _ in range(4)]
+assembly_stations_rotations = [quaternion_from_euler(0,0,0) for _ in range(4)]
 
 for i in range(1,5):
     objects_dict[f"as{i}"] = {"position":assembly_stations_positions[i-1],"orientation":assembly_stations_rotations[i-1],"file":"assembly_station.stl"}
 
+
 # ASSEMBLY BRIEFCASES
-assembly_briefcase_positions = [[x,y,0.0]
+assembly_insert_positions = [[x,y,1.011]
                                 for x in [-7.7,-12.7]
                                 for y in [3.0,-3.0]]
-assembly_briefcase_rotations = [[0.0,0.0,0.0,1.0] for _ in range(4)]
 
 for i in range(1,5):
-    objects_dict[f"assembly_insert{i}"] = {"position":assembly_briefcase_positions[i-1],"orientation":assembly_briefcase_rotations[i-1],"file":"assembly_insert.stl"}
+    objects_dict[f"assembly_insert{i}"] = {"position":assembly_insert_positions[i-1],"orientation":quaternion_from_euler(0,0,0),"file":"assembly_insert.stl"}
 
 # CONVEYOR BELT
-objects_dict["conveyor"] = {"position":[-0.6,0,0], "orientation":[0.0,0.0,0.0,1.0], "file":"conveyor.stl"}
+objects_dict["conveyor"] = {"position":[-0.6,0,0], "orientation":quaternion_from_euler(0,0,0), "file":"conveyor.stl"}
 
 # KITTING TABLES
 kitting_table_positions = [[-1.3,y,0.0] for y in [5.84,-5.84]]
-kitting_table_rotations = [[0.0,0.0,0.0,1.0] for _ in range(2)]
+kitting_table_rotations = [quaternion_from_euler(0,0,0),quaternion_from_euler(0,0,math.pi)]
 
-for i in range(1,3):
-    objects_dict[f"kts{i}_table"] = {"position":kitting_table_positions[i-1],"orientation":kitting_table_rotations[i-1],"file":"kit_tray_table.stl"}
-
+objects_dict["kts1_table"] = {"position":[-1.3,5.84,0.0],"orientation":quaternion_from_euler(0,0,0),"file":"kit_tray_table.stl"}
+objects_dict["kts2_table"] = {"position":[-1.3,-5.84,0.0],"orientation":quaternion_from_euler(0,0,math.pi),"file":"kit_tray_table.stl"}
 print("The python dictionary is:")
 print(objects_dict)
 file=open("collision_object_info.yaml","w")
