6f36a55dc451de30b479b8088a1e26a2a97daccc
Joseph Fernandez <jothomas0615@gmail.com>
2024-01-04 13:00:54 -0500

    Small changes for reliablity

--- ariac_tutorials/competition_interface.py
+++ ariac_tutorials/competition_interface.py
@@ -1320,8 +1320,8 @@ class CompetitionInterface(Node):
             self.get_logger().error("No part attached")
             return False
 
-        self.floor_robot_move_joints_dict({"linear_actuator_joint":self._rail_positions[f"agv{agv_num}"],
-                                       "floor_shoulder_pan_joint":0})
+        # self.floor_robot_move_joints_dict({"linear_actuator_joint":self._rail_positions[f"agv{agv_num}"],
+        #                                "floor_shoulder_pan_joint":0})
         
         agv_tray_pose = self._frame_world_pose(f"agv{agv_num}_tray")
 
@@ -1338,7 +1338,7 @@ class CompetitionInterface(Node):
                                 part_drop_pose.position.z+CompetitionInterface._part_heights[self.floor_robot_attached_part_.type]+0.01, 
                                 quaternion_from_euler(0.0, pi, 0.0))]
         
-        self._move_floor_robot_cartesian(waypoints, 0.3, 0.3,)
+        self._move_floor_robot_cartesian(waypoints, 0.3, 0.3,False)
 
         self.set_floor_robot_gripper_state(False)
 
@@ -1348,7 +1348,7 @@ class CompetitionInterface(Node):
                                 part_drop_pose.position.z+0.3, 
                                 quaternion_from_euler(0.0, pi, 0.0))]
         
-        self._move_floor_robot_cartesian(waypoints, 0.3, 0.3)
+        self._move_floor_robot_cartesian(waypoints, 0.3, 0.3,False)
 
         return True
 
